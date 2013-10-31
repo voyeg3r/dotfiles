dcd1895332e39efb87357aa7a688a7c87a07a462
voyeg3r <voyeg3r@gmail.com>
2013-10-31 05:58:29 -0300

    updated script to change wallpaper

--- bin/changewallpaper.sh
+++ bin/changewallpaper.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
-# http://stackoverflow.com/questions/10374520/gsettings-with-cron
-# see also: ~/.cache/gnome-control-center/backgrounds
-# http://superuser.com/questions/298050/periodically-changing-wallpaper-under-gnome-3
-# http://dsathe.blogspot.com.br/2011/06/auto-background-changer-for-gnome-3-gui.html
-
+# source: http://stackoverflow.com/questions/10374520/gsettings-with-cron
 # TODO: At night only dark wallpapers.
-#   Hora=$(date +%H)
-#   case $Hora in
-#              0? | 1[01]) dir=/caminho/para/folder/manha ;;
-#              1[2-7] )  dir=/caminho/para/folder/tarde ;;
-#               * ) dir=/caminho/para/folder/noite ;;
-#   esac
 
 # Wallpaper's directory.
-dir="${HOME}/img/wallpapers/"
+dir="${HOME}/Pictures/wallpapers/"
 
-# Weird, but necessary thing to run with cron.
-sessionfile=`find "${HOME}/.dbus/session-bus/" -type f`
-export `grep "DBUS_SESSION_BUS_ADDRESS" "${sessionfile}" | sed '/^#/d'`
+# export DBUS_SESSION_BUS_ADDRESS environment variable
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 # Random wallpaper.
 wallpaper=`find "${dir}" -type f | shuf -n1`
 
-echo "trying change wallpaper to $wallpaper"
-
 # Change wallpaper.
 # http://bit.ly/HYEU9H
-#gsettings set org.gnome.desktop.background picture-options "spanned"
-gsettings set org.gnome.desktop.background picture-options "stretched"
+gsettings set org.gnome.desktop.background picture-options "spanned"
 gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
+
