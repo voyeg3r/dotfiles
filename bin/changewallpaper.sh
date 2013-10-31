e6e7881059da1a10d24c329de7f90358e33d98d5
voyeg3r <voyeg3r@gmail.com>
2013-10-31 06:17:17 -0300

    little correction in one word of script

--- bin/changewallpaper.sh
+++ bin/changewallpaper.sh
@@ -15,6 +15,6 @@ wallpaper=`find "${dir}" -type f | shuf -n1`
 #Determines how the image set by wallpaper_filename is rendered. Possible values are "none", "wallpaper", "centered", "scaled", "stretched", "zoom", "spanned".
 
 #gsettings set org.gnome.desktop.background picture-options "spanned"
-gsettings set org.gnome.desktop.background picture-options "scalled"
+gsettings set org.gnome.desktop.background picture-options "scaled"
 gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
 
