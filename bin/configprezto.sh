4470c870e72bbebe3ed44c9343a1e3364f6fd2b1
voyeg3r <voyeg3r@gmail.com>
2015-02-18 08:27:58 -0300

    trying to update all changes

--- bin/configprezto.sh
+++ bin/configprezto.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env zsh
-# =================================================
-# CREATED:          Sáb 29/Dez/2012 hs 10:25
-# LAST CHANGE: Fri Feb 13, 2015  03:16PM
-# THIS SCRIPT AIMS: describe here
-# AUTHOR:           Sérgio Luiz Araújo Silva
-# SITE:             http://vivaotux.blogspot.com
-# TWITTER:          @voyeg3r
-# SKYPE:            sergioaraujosilva
-# =================================================
 
-ZDOTDIR=~/.dotfiles
+# clone dir
+DOTDIR=~/.dotfiles
 
-git clone --recursive https://github.com/voyeg3r/prezto "${ZDOTDIR:-$HOME}/zprezto"
+git clone --recursive git@github.com:voyeg3r/prezto.git "${ZDOTDIR:-$HOME}/zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/zprezto/runcoms/^README.md(.N); do
-    ln -sfvn "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
+  ln -sfvn "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
 done
 
-cd "${ZDOTDIR:-$HOME}"/zprezto/
-find -type f | xargs sed -i 's/\.zprezto/zprezto/g'
+
