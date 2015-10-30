787a35e51876bce7cc81e5f9c397117e9f42345e
voyeg3r <voyeg3r@gmail.com>
2015-10-30 08:12:26 -0300

    change wallpaper script

--- bin/changewallpaper.sh
+++ bin/changewallpaper.sh
@@ -8,18 +8,24 @@
 # Tip: Don't forget create one blank line at bottom in crontab file
 
 # Wallpaper's directory.
-dir="${HOME}/docs/img/wallpapers"
+# dir="${HOME}/pictures/new-wallpapers/
+#
+# # export DBUS_SESSION_BUS_ADDRESS environment variable
+# PID=$(pgrep gnome-session)
+# export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+#
+# # Random wallpaper.
+# wallpaper=`find "${dir}" -type f | shuf -n1`
+#
+# #Determines how the image set by wallpaper_filename is rendered. Possible values are "none", "wallpaper", "centered", "scaled", "stretched", "zoom", "spanned".
+#
+# #gsettings set org.gnome.desktop.background picture-options "spanned"
+# gsettings set org.gnome.desktop.background picture-options "stretched"
+# gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
 
-# export DBUS_SESSION_BUS_ADDRESS environment variable
-PID=$(pgrep gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+# new reference: https://major.io/2015/02/11/rotate-gnome-3s-wallpaper-systemd-user-units-timers/
 
-# Random wallpaper.
-wallpaper=`find "${dir}" -type f | shuf -n1`
 
-#Determines how the image set by wallpaper_filename is rendered. Possible values are "none", "wallpaper", "centered", "scaled", "stretched", "zoom", "spanned".
-
-#gsettings set org.gnome.desktop.background picture-options "spanned"
-gsettings set org.gnome.desktop.background picture-options "stretched"
-gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper}"
-
+walls_dir=$HOME/Pictures/Wallpapers
+selection=$(find $walls_dir -type f -name "*.jpg" -o -name "*.png" | shuf -n1)
+gsettings set org.gnome.desktop.background picture-uri "file://$selection"
