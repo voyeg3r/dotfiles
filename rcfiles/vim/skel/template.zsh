d42135dd2e118b3834a811acf1d2a1859b22fd75
voyeg3r <voyeg3r@gmail.com>
2016-09-22 11:38:03 -0300

    a lot of new tricks and a new amazing script

---
+++ rcfiles/vim/skel/template.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+# =================================================
+# CREATED:
+# LAST CHANGE:
+# THIS SCRIPT AIMS: describe here
+# AUTHOR:           Sérgio Luiz Araújo Silva
+# SITE:             http://vivaotux.blogspot.com
+# TWITTER:          @voyeg3r
+# SKYPE:            sergioaraujosilva
+# =================================================
+
