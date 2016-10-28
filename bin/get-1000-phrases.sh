722d3835c472e62665c3a66986b3bd1c19d4bbb2
voyeg3r <voyeg3r@gmail.com>
2016-10-28 16:36:40 -0300

    today I have learned a lot about linux commands

--- bin/get-1000-phrases.sh
+++ bin/get-1000-phrases.sh
@@ -9,25 +9,67 @@
 # SKYPE:            sergioaraujosilva
 # =================================================
 
-URL="http://www.englishspeak.com/pt/english-phrases.cfm\?newCategoryShowed\="
-COMPLEMENT="\&sortBy\=28"
 
 
 # o comando abaixo pega o texto
 
 # lynx -dump -display_charset UTF-8 http://www.englishspeak.com/pt/english-phrases.cfm\?newCategoryShowed\=17\&sortBy\=28 | sed 's/\[[^]]*\]//g' | sed -ne '/Esconder/,/[Hh]ome/p' | sed '/Sentanças mais comuns/d' | awk 'NF > 0'
 
+# url dos arquivos de audio mp3: http://www.englishspeak.com/instantspeak/English/phrases/mp3/
+
+# duplicar a segunda linha: ............... cat text-001.txt | sed '2~2p'
+# fazer a terceira linha lowcase: ......... sed '3~3s/\(.*\)/\L\1/'
+# juntar três linhas consecutivas: ........ sed 'N;N;s/\n/ /g'
+
+# cat text-001.txt | sed '2~2p' | sed '3~3s/\(.*\)/\L\1/' | sed '3~3s/[[:punct:]]//g' | sed '3~3s, ,,g ; 3~3s,$,.mp3,g ; 3~3s,.*,[sound:&],g' | sed 'N;N;s/\n/ /g'
+
+# endereço 1: http://www.englishspeak.com/pt/english-phrases.cfm?newCategoryShowed=1&sortBy=28
+# endereço 2: http://www.englishspeak.com/pt/english-phrases.cfm?newCategoryShowed=2&sortBy=28
+
+
+# AUDIO="http://www.englishspeak.com/instantspeak/English/phrases/mp3/"
+# URL='http://www.englishspeak.com/pt/english-phrases.cfm\?newCategoryShowed\='
+# COMPLEMENT='\&sortBy\=28'
+
 clear
 
-for ((i=1;i<=19;i++)){
-    echo Criando a pasta $(printf "%03d\n" $i) ...
-    echo Entrando na pasta ...
-    mkdir folder-$(printf "%03d\n" $i) && cd $_
-    echo Obtendo o arquivo $(printf "%03d\n" $i) ...
-    lynx -dump -display_charset UTF-8 ${URL}${i}${COMPLEMENT} | sed 's/\[[^]]*\]//g' | sed -ne '/Esconder/,/[Hh]ome/p' | sed '/Sentanças mais comuns/d' | awk 'NF > 0' > text-$(printf "%03d\n" $i).txt
+for ((i=1;i<=18;i++)){
 
-     cd ..
-}
+     AUDIO="http://www.englishspeak.com/instantspeak/English/phrases/mp3/"
+     URL='http://www.englishspeak.com/pt/english-phrases.cfm?newCategoryShowed='
+     COMPLEMENT='&sortBy=28'
+
+     NUMBER=$(printf "%03d\n" $i)
+
+     ADDRESS="${URL}${i}${COMPLEMENT}"
+
+     mkdir folder-$NUMBER && cd $_
+
+      echo "Baixando $ADDRESS"
+      lynx -dump -display_charset UTF-8 $ADDRESS | sed 's/\[[^]]*\]//g' | sed -ne '/Esconder/,/[Hh]ome/p' | sed '/Sentanças mais comuns/d' | awk 'NF > 0' > text-${NUMBER}.txt
+
+       echo removendo as cinco primeiras linhas
+       sed -i '1,5d' text-${NUMBER}.txt
+       sed -i 's/^\s\+//g' text-${NUMBER}.txt
+
+       cat text-${NUMBER}.txt | sed '2~2p' | sed '3~3s/\(.*\)/\L\1/' | sed '3~3s/[[:punct:]]//g ; 3~3s/ //g ; 3~3s/$/.mp3/g ; 3~3s/.*/[sound:&]/g' | sed '1~3s/$/;/g' | sed 'N;N;s/\n/ /g' > deck.csv
+
+       cat text-${NUMBER}.txt | sed -n '1~2!p' | tr -d '[:punct:]' | sed 's, ,,g' | sed 's,$,.mp3,' | tr '[:upper:]' '[:lower:]' > download.txt
+
+       sed -i "s,^,$AUDIO,g" download.txt
+
+       mkdir mp3 && cd $_
+
+       echo "Baixando os audios..."
+
+       wget -c -i ../download.txt
+
+       cd ..
+
+      cd ..
+
+      clear
+ }
 
 
 
