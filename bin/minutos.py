9c3fef5c5712d2fbd3f8e8d657c024634058210c
voyeg3r <voyeg3r@gmail.com>
2017-01-27 16:29:53 -0300

    little change on my script

--- bin/minutos.py
+++ bin/minutos.py
@@ -27,13 +27,12 @@ def calcular():
     minutos = int(input("Minutos utilizados: "))
     if minutos < 200:
         preço = 0.20
+    elif minutos <= 400:
+        preço = 0.18
+    elif minutos <= 800 :
+        preço = 0.15
     else:
-        if minutos <= 400:
-            preço = 0.18
-        elif minutos <= 800 :
-            preço = 0.15
-        else:
-            preço = 0.08
+        preço = 0.08
 
     print(f"Conta telefonica: R$ {minutos * preço:.02f}")
 
