140b41255a6a5403d7af5033df38476d419f11b0
voyeg3r <voyeg3r@gmail.com>
2017-06-20 01:15:38 -0300

    two more change wallpaper scripts

---
+++ bin/changewallpaper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# # -*- coding: UTF-8 -*-"
+# ------------------------------------------------
+#   Creation Date:    19-06-2017
+#     Last Change:  ter 29 nov 2016 09:21:52 BRT
+#            File:  <+File Name+>
+#          author:  sergio luiz araujo silva
+#            site:  http://vivaotux.blogspot.com
+#         twitter:  @voyeg3r
+# ------------------------------------------------
+
+import os
+import random
+import mimetypes
+
+backgrounds = os.environ['HOME'] + "/img/new-wallpapers/"
+items = []
+
+for item in os.listdir(backgrounds):
+        if "image" in mimetypes.guess_type(item)[0]:
+                items.append (item)
+
+
+item = random.randrange (0, len (items))
+os.system('gsettings set org.gnome.desktop.background picture-uri "file:///' + backgrounds + items[item] + '"' )
+
+
